a45fed740a2bf767d2e731e07093b8c2ea6e18f8
Thryn <52488475+ThrynSec@users.noreply.github.com>
2020-05-25 23:50:20 +0200

    Add files via upload

---
+++ Discord Health/FormBase.Designer.vb
@@ -0,0 +1,82 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormBase
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormBase))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BtnCheck = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Calibri", 27.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(50, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(350, 45)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Discord Virus Checker"
+        '
+        'BtnCheck
+        '
+        Me.BtnCheck.Font = New System.Drawing.Font("Calibri", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnCheck.Location = New System.Drawing.Point(106, 117)
+        Me.BtnCheck.Name = "BtnCheck"
+        Me.BtnCheck.Size = New System.Drawing.Size(260, 46)
+        Me.BtnCheck.TabIndex = 1
+        Me.BtnCheck.Text = "Check if Discord is infected"
+        Me.BtnCheck.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(335, 183)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(114, 98)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'FormBase
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(461, 290)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.BtnCheck)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "FormBase"
+        Me.Text = "Discord Virus Checker"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents BtnCheck As Button
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Discord Health/FormBase.vb
@@ -0,0 +1,67 @@
+﻿Imports System.IO
+Imports System.Text
+Imports System.Security.Principal
+Public Class FormBase
+
+    Dim versionName As String, infection As Integer
+    Dim identity = WindowsIdentity.GetCurrent()
+    Dim program = New WindowsPrincipal(identity)
+    Dim isAdmin As Boolean = program.IsInRole(WindowsBuiltInRole.Administrator)
+
+    Private Sub LaunchCheck() Handles BtnCheck.Click
+        Dim sessionUserName As String = Environment.UserName
+        For Each Dir As String In System.IO.Directory.GetDirectories("C:\Users\" & sessionUserName & "\AppData\Roaming\Discord")
+            Dim dirInfo As New System.IO.DirectoryInfo(Dir)
+            If Strings.Left(dirInfo.Name, 1) = "0" Or
+                Strings.Left(dirInfo.Name, 1) = "1" Or
+                Strings.Left(dirInfo.Name, 1) = "2" Or
+                Strings.Left(dirInfo.Name, 1) = "3" Or
+                Strings.Left(dirInfo.Name, 1) = "4" Or
+                Strings.Left(dirInfo.Name, 1) = "5" Or
+                Strings.Left(dirInfo.Name, 1) = "6" Or
+                Strings.Left(dirInfo.Name, 1) = "7" Or
+                Strings.Left(dirInfo.Name, 1) = "9" Or
+                Strings.Left(dirInfo.Name, 1) = "8" Then
+                versionName = dirInfo.Name
+            End If
+        Next
+
+        Using sr As StreamReader = File.OpenText("C:\Users\" & sessionUserName & "\AppData\Roaming\Discord\" & versionName & "\modules\discord_modules\index.js")
+            If sr.ReadLine() <> "module.exports = require('./discord_modules.node');" Then
+                infection = 1
+            Else
+                infection = 0
+            End If
+        End Using
+
+        Using sr As StreamReader = File.OpenText("C:\Users\" & sessionUserName & "\AppData\Roaming\Discord\" & versionName & "\modules\discord_desktop_core\index.js")
+            If sr.ReadLine() <> "module.exports = require('./core.asar');" Then
+                If infection = 1 Then
+                    infection = 3
+                Else
+                    infection = 2
+                End If
+            Else
+                infection = 0
+            End If
+        End Using
+
+        Select Case infection
+            Case 1
+                MsgBox("The file 'C:\Users\" & sessionUserName & "\AppData\Roaming\Discord\" & versionName & "\modules\discord_modules\index.js' have been infected." &
+                       Chr(10) & Chr(10) & "To avoid stealing of your accounts, passwords, copyboard and so on... Please reinstall Discord (desktop version)")
+
+            Case 2
+                MsgBox("The file 'C:\Users\" & sessionUserName & "\AppData\Roaming\Discord\" & versionName & "\modules\discord_desktop_core\index.js' have been infected." &
+                       Chr(10) & Chr(10) & "To avoid stealing of your accounts, passwords, copyboard and so on... Please reinstall Discord (desktop version)")
+
+            Case 3
+                MsgBox("Both files 'C:\Users\" & sessionUserName & "\AppData\Roaming\Discord\" & versionName & "\modules\discord_desktop_core\index.js' et 'C:\Users\" & sessionUserName & "\AppData\Roaming\Discord\" & versionName & "\modules\discord_modules\index.js' have been infected." &
+                       Chr(10) & Chr(10) & "To avoid stealing of your accounts, passwords, copyboard and so on... Please reinstall Discord (desktop version)")
+            Case 0
+                MsgBox("Your Discord installation have not been compromised, no need to reinstall it." & Chr(10) & "Please stay aware of the links you click on and the servers you may join.")
+        End Select
+
+    End Sub
+
+End Class
